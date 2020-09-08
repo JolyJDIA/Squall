381b933a4cda0aeb2c9809046a2dc080c0ba5b42
BuildTools <unconfigured@null.spigotmc.org>
2020-09-08 20:35:07 +0300

    squall0

--- src/main/java/jolyjdia/test/util/squall/function/BiConsumerResultSet.java
+++
@@ -1,9 +0,0 @@
-package jolyjdia.test.util.squall.function;
-
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
-@FunctionalInterface
-public interface BiConsumerResultSet<T> {
-    void accept(T t, ResultSet rs) throws SQLException;
-}
--- src/main/java/jolyjdia/test/util/squall/function/ConsumerResultSet.java
+++
@@ -1,9 +0,0 @@
-package jolyjdia.test.util.squall.function;
-
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
-@FunctionalInterface
-public interface ConsumerResultSet {
-    void accept(ResultSet rs) throws SQLException;
-}
